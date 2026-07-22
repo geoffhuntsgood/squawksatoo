266f9841e46043d9726e1b32b2309b6cecd8603a
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-07-22 17:38:48 -0400

    test fixes, remove old images

--- src/test/layers/layerApi.test.ts
+++ src/test/layers/layerApi.test.ts
@@ -9,136 +9,153 @@ import {
 
 describe("layerApi tests", () => {
   describe("getLayerBananas tests", () => {
+    test("All", () => {
+      const all = getLayerBananas(LayerName.All, true, true);
+      expect(all.length).toBe(777);
+    });
+
     test("Ingot", () => {
-      const ingot = getLayerBananas(LayerName.Ingot);
+      const ingot = getLayerBananas(LayerName.Ingot, false, false);
       expect(ingot.length).toBe(3);
-      const ingotPostgame = getLayerBananas(LayerName.Ingot, true);
+      const ingotPostgame = getLayerBananas(LayerName.Ingot, true, false);
       expect(ingotPostgame.length).toBe(36);
     });
 
     test("Lagoon", () => {
-      const lagoon = getLayerBananas(LayerName.Lagoon);
+      const lagoon = getLayerBananas(LayerName.Lagoon, false, false);
       expect(lagoon.length).toBe(43);
-      const lagoonPostgame = getLayerBananas(LayerName.Lagoon, true);
+      const lagoonPostgame = getLayerBananas(LayerName.Lagoon, true, false);
       expect(lagoonPostgame.length).toBe(56);
     });
 
     test("Hilltop", () => {
-      const hilltop = getLayerBananas(LayerName.Hilltop);
+      const hilltop = getLayerBananas(LayerName.Hilltop, false, false);
       expect(hilltop.length).toBe(35);
-      const hilltopPostgame = getLayerBananas(LayerName.Hilltop, true);
+      const hilltopPostgame = getLayerBananas(LayerName.Hilltop, true, false);
       expect(hilltopPostgame.length).toBe(38);
     });
 
     test("Canyon", () => {
-      const canyon = getLayerBananas(LayerName.Canyon);
+      const canyon = getLayerBananas(LayerName.Canyon, false, false);
       expect(canyon.length).toBe(79);
-      const canyonPostgame = getLayerBananas(LayerName.Canyon, true);
+      const canyonPostgame = getLayerBananas(LayerName.Canyon, true, false);
       expect(canyonPostgame.length).toBe(80);
     });
 
     test("Divide", () => {
-      const divide = getLayerBananas(LayerName.Divide);
+      const divide = getLayerBananas(LayerName.Divide, false, false);
       expect(divide.length).toBe(9);
-      const dividePostgame = getLayerBananas(LayerName.Divide, true);
+      const dividePostgame = getLayerBananas(LayerName.Divide, true, false);
       expect(dividePostgame.length).toBe(10);
     });
 
     test("Freezer", () => {
-      const freezer = getLayerBananas(LayerName.Freezer);
+      const freezer = getLayerBananas(LayerName.Freezer, false, false);
       expect(freezer.length).toBe(72);
-      const freezerPostgame = getLayerBananas(LayerName.Freezer, true);
+      const freezerPostgame = getLayerBananas(LayerName.Freezer, true, false);
       expect(freezerPostgame.length).toBe(87);
     });
 
     test("Forest", () => {
-      const forest = getLayerBananas(LayerName.Forest);
+      const forest = getLayerBananas(LayerName.Forest, false, false);
       expect(forest.length).toBe(74);
-      const forestPostgame = getLayerBananas(LayerName.Forest, true);
+      const forestPostgame = getLayerBananas(LayerName.Forest, true, false);
       expect(forestPostgame.length).toBe(83);
     });
 
     test("Junction", () => {
-      const junction = getLayerBananas(LayerName.Junction);
+      const junction = getLayerBananas(LayerName.Junction, false, false);
       expect(junction.length).toBe(13);
-      const junctionPostgame = getLayerBananas(LayerName.Junction, true);
+      const junctionPostgame = getLayerBananas(LayerName.Junction, true, false);
       expect(junctionPostgame.length).toBe(15);
     });
 
     test("Resort", () => {
-      const resort = getLayerBananas(LayerName.Resort);
+      const resort = getLayerBananas(LayerName.Resort, false, false);
       expect(resort.length).toBe(41);
-      const resortPostgame = getLayerBananas(LayerName.Resort, true);
+      const resortPostgame = getLayerBananas(LayerName.Resort, true, false);
       expect(resortPostgame.length).toBe(42);
     });
 
     test("Tempest", () => {
-      const tempest = getLayerBananas(LayerName.Tempest);
+      const tempest = getLayerBananas(LayerName.Tempest, false, false);
       expect(tempest.length).toBe(62);
-      const tempestPostgame = getLayerBananas(LayerName.Tempest, true);
+      const tempestPostgame = getLayerBananas(LayerName.Tempest, true, false);
       expect(tempestPostgame.length).toBe(70);
     });
 
     test("Landfill", () => {
-      const landfill = getLayerBananas(LayerName.Landfill);
+      const landfill = getLayerBananas(LayerName.Landfill, false, false);
       expect(landfill.length).toBe(26);
-      const landfillPostgame = getLayerBananas(LayerName.Landfill, true);
+      const landfillPostgame = getLayerBananas(LayerName.Landfill, true, false);
       expect(landfillPostgame.length).toBe(27);
     });
 
     test("Racing", () => {
-      const racing = getLayerBananas(LayerName.Racing);
+      const racing = getLayerBananas(LayerName.Racing, false, false);
       expect(racing.length).toBe(9);
-      const racingPostgame = getLayerBananas(LayerName.Racing, true);
+      const racingPostgame = getLayerBananas(LayerName.Racing, true, false);
       expect(racingPostgame.length).toBe(10);
     });
 
     test("Radiance", () => {
-      const radiance = getLayerBananas(LayerName.Radiance);
+      const radiance = getLayerBananas(LayerName.Radiance, false, false);
       expect(radiance.length).toBe(67);
-      const radiancePostgame = getLayerBananas(LayerName.Radiance, true);
+      const radiancePostgame = getLayerBananas(LayerName.Radiance, true, false);
       expect(radiancePostgame.length).toBe(76);
     });
 
-    test("Groove", () => {
-      const groove = getLayerBananas(LayerName.Groove);
+    test("Groove (with bonus I hate myself)", () => {
+      const groove = getLayerBananas(LayerName.Groove, false, false);
       expect(groove.length).toBe(26);
-      const groovePostgame = getLayerBananas(LayerName.Groove, true);
+      const groovePostgame = getLayerBananas(LayerName.Groove, true, true);
       expect(groovePostgame.length).toBe(30);
     });
 
     test("Feast", () => {
-      const feast = getLayerBananas(LayerName.Feast);
+      const feast = getLayerBananas(LayerName.Feast, false, false);
       expect(feast.length).toBe(67);
-      const feastPostgame = getLayerBananas(LayerName.Feast, true);
+      const feastPostgame = getLayerBananas(LayerName.Feast, true, false);
       expect(feastPostgame.length).toBe(69);
     });
 
     test("Forbidden", () => {
-      const forbidden = getLayerBananas(LayerName.Forbidden);
+      const forbidden = getLayerBananas(LayerName.Forbidden, false, false);
       expect(forbidden.length).toBe(24);
-      const forbiddenPostgame = getLayerBananas(LayerName.Forbidden, true);
+      const forbiddenPostgame = getLayerBananas(
+        LayerName.Forbidden,
+        true,
+        false
+      );
       expect(forbiddenPostgame.length).toBe(25);
     });
 
     test("Core", () => {
-      const core = getLayerBananas(LayerName.Core);
+      const core = getLayerBananas(LayerName.Core, false, false);
       expect(core.length).toBe(20);
-      const corePostgame = getLayerBananas(LayerName.Core, true);
+      const corePostgame = getLayerBananas(LayerName.Core, true, false);
       expect(corePostgame.length).toBe(23);
     });
   });
 
   describe("getAllBananasForCategories tests", () => {
     test("No categories, no postgame", () => {
-      const feast = getAllBananasForCategories(LayerName.Feast, []);
+      const feast = getAllBananasForCategories(
+        LayerName.Feast,
+        [],
+        false,
+        false
+      );
       expect(feast.length).toBe(67);
     });
 
     test("One category, no postgame", () => {
-      const canyon = getAllBananasForCategories(LayerName.Canyon, [
-        DKBCategory.Fragmentone
-      ]);
+      const canyon = getAllBananasForCategories(
+        LayerName.Canyon,
+        [DKBCategory.Fragmentone],
+        false,
+        false
+      );
       expect(canyon.length).toBe(2);
     });
 
@@ -146,7 +163,8 @@ describe("layerApi tests", () => {
       const radiance = getAllBananasForCategories(
         LayerName.Radiance,
         [DKBCategory.Battle, DKBCategory.Rehearsal],
-        true
+        true,
+        false
       );
       expect(radiance.length).toBe(15);
       expect(
--- src/test/levels/levelApi.test.ts
+++ src/test/levels/levelApi.test.ts
@@ -6,32 +6,39 @@ import { getAllCollectablesForCategories } from "../../levels/levelApi";
 
 describe("levelApi tests", () => {
   describe("getAllCollectablesForCategories tests", () => {
-    test("No category, all levels", () => {
-      const allCollectables = getAllCollectablesForCategories([], LevelName.All);
-      expect(allCollectables.length).toBe(280);
+    test("No category, all levels, I don't hate myself", () => {
+      const allCollectables = getAllCollectablesForCategories(
+        [],
+        LevelName.All,
+        false
+      );
+      expect(allCollectables.length).toBe(410);
     });
 
-    test("One category, all levels", () => {
-      const allFairies = getAllCollectablesForCategories([DK64Category.Fairy], LevelName.All);
-      expect(allFairies.length).toBe(19);
+    test("One category, all levels, I do hate myself", () => {
+      const allGBs = getAllCollectablesForCategories(
+        [DK64Category.GB],
+        LevelName.All,
+        true
+      );
+      expect(allGBs.length).toBe(201);
     });
 
     test("Multiple categories with level", () => {
       const factory = getAllCollectablesForCategories(
         [DK64Category.GB, DK64Category.Medal],
-        LevelName.Factory
+        LevelName.Factory,
+        false
       );
       expect(factory.length).toBe(30);
       expect(
         factory.filter(
-          (collectable: DK64Item) =>
-            collectable.category === DK64Category.GB
+          (collectable: DK64Item) => collectable.category === DK64Category.GB
         ).length
       ).toBe(25);
       expect(
         factory.filter(
-          (collectable: DK64Item) =>
-            collectable.category === DK64Category.Medal
+          (collectable: DK64Item) => collectable.category === DK64Category.Medal
         ).length
       ).toBe(5);
     });
