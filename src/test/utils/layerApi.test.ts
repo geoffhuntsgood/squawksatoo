950da8c2d21308a45caeba17da037161981443f3
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-07-27 22:32:46 -0400

    iterating tests

--- src/test/utils/layerApi.test.ts
+++ src/test/utils/layerApi.test.ts
@@ -8,348 +8,71 @@ import {
 
 describe("layerApi tests", () => {
   describe("getBananasForLayer tests", () => {
-    test("All", () => {
-      expect(getBananasForLayer(LayerName.All)).toHaveLength(777);
-    });
+    const expected = {
+      [LayerName.Ingot]: 36,
+      [LayerName.Lagoon]: 56,
+      [LayerName.Hilltop]: 38,
+      [LayerName.Canyon]: 80,
+      [LayerName.Divide]: 10,
+      [LayerName.Freezer]: 87,
+      [LayerName.Forest]: 83,
+      [LayerName.Junction]: 15,
+      [LayerName.Resort]: 42,
+      [LayerName.Tempest]: 70,
+      [LayerName.Landfill]: 27,
+      [LayerName.Racing]: 10,
+      [LayerName.Radiance]: 76,
+      [LayerName.Groove]: 30,
+      [LayerName.Feast]: 69,
+      [LayerName.Forbidden]: 25,
+      [LayerName.Core]: 23,
+      [LayerName.All]: 777
+    };
 
-    test("Ingot", () => {
-      expect(getBananasForLayer(LayerName.Ingot)).toHaveLength(36);
-    });
-
-    test("Lagoon", () => {
-      expect(getBananasForLayer(LayerName.Lagoon)).toHaveLength(56);
-    });
-
-    test("Hilltop", () => {
-      expect(getBananasForLayer(LayerName.Hilltop)).toHaveLength(38);
-    });
-
-    test("Canyon", () => {
-      expect(getBananasForLayer(LayerName.Canyon)).toHaveLength(80);
-    });
-
-    test("Divide", () => {
-      expect(getBananasForLayer(LayerName.Divide)).toHaveLength(10);
-    });
-
-    test("Freezer", () => {
-      expect(getBananasForLayer(LayerName.Freezer)).toHaveLength(87);
-    });
-
-    test("Forest", () => {
-      expect(getBananasForLayer(LayerName.Forest)).toHaveLength(83);
-    });
-
-    test("Junction", () => {
-      expect(getBananasForLayer(LayerName.Junction)).toHaveLength(15);
-    });
-
-    test("Resort", () => {
-      expect(getBananasForLayer(LayerName.Resort)).toHaveLength(42);
-    });
-
-    test("Tempest", () => {
-      expect(getBananasForLayer(LayerName.Tempest)).toHaveLength(70);
-    });
-
-    test("Landfill", () => {
-      expect(getBananasForLayer(LayerName.Landfill)).toHaveLength(27);
-    });
-
-    test("Racing", () => {
-      expect(getBananasForLayer(LayerName.Racing)).toHaveLength(10);
-    });
-
-    test("Radiance", () => {
-      expect(getBananasForLayer(LayerName.Radiance)).toHaveLength(76);
-    });
-
-    test("Groove", () => {
-      expect(getBananasForLayer(LayerName.Groove)).toHaveLength(30);
-    });
-
-    test("Feast", () => {
-      expect(getBananasForLayer(LayerName.Feast)).toHaveLength(69);
-    });
-
-    test("Forbidden", () => {
-      expect(getBananasForLayer(LayerName.Forbidden)).toHaveLength(25);
-    });
-
-    test("Core", () => {
-      expect(getBananasForLayer(LayerName.Core)).toHaveLength(23);
+    Object.values(LayerName).forEach((layer: LayerName) => {
+      test(`${layer}`, () => {
+        expect(getBananasForLayer(layer)).toHaveLength(expected[layer]);
+      });
     });
   });
 
   describe("getCategoriesForLayer tests", () => {
-    test("Ingot", () => {
-      expect(getCategoriesForLayer(LayerName.Ingot)).toEqual([
-        DKBCategory.Tutorial,
-        DKBCategory.Regular,
-        DKBCategory.Triple,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Fragmentone,
-        DKBCategory.Growtone,
-        DKBCategory.Seekertone,
-        DKBCategory.Smashintone,
-        DKBCategory.Quiztone,
-        DKBCategory.Glamtone,
-        DKBCategory.Cranky,
-        DKBCategory.Quintuple,
-        DKBCategory.Battle,
-        DKBCategory.Chiptone
-      ]);
-    });
+    const expected = {
+      [LayerName.Ingot]: 14,
+      [LayerName.Lagoon]: 10,
+      [LayerName.Hilltop]: 13,
+      [LayerName.Canyon]: 12,
+      [LayerName.Divide]: 7,
+      [LayerName.Freezer]: 12,
+      [LayerName.Forest]: 12,
+      [LayerName.Junction]: 6,
+      [LayerName.Resort]: 14,
+      [LayerName.Tempest]: 11,
+      [LayerName.Landfill]: 10,
+      [LayerName.Racing]: 6,
+      [LayerName.Radiance]: 12,
+      [LayerName.Groove]: 7,
+      [LayerName.Feast]: 13,
+      [LayerName.Forbidden]: 7,
+      [LayerName.Core]: 8,
+      [LayerName.All]: 17
+    };
 
-    test("Lagoon", () => {
-      expect(getCategoriesForLayer(LayerName.Lagoon)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Regular,
-        DKBCategory.Smashintone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Battle,
-        DKBCategory.Cranky,
-        DKBCategory.Challenge,
-        DKBCategory.Chiptone,
-        DKBCategory.Rehearsal,
-        DKBCategory.Seekertone
-      ]);
-    });
-
-    test("Hilltop", () => {
-      expect(getCategoriesForLayer(LayerName.Hilltop)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Regular,
-        DKBCategory.Battle,
-        DKBCategory.Challenge,
-        DKBCategory.Fragmentone,
-        DKBCategory.Seekertone,
-        DKBCategory.Smashintone,
-        DKBCategory.Bloomintone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Quiztone,
-        DKBCategory.Cranky,
-        DKBCategory.Chiptone,
-        DKBCategory.Growtone
-      ]);
-    });
-
-    test("Canyon", () => {
-      expect(getCategoriesForLayer(LayerName.Canyon)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Regular,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Battle,
-        DKBCategory.Challenge,
-        DKBCategory.Fragmentone,
-        DKBCategory.Smashintone,
-        DKBCategory.Growtone,
-        DKBCategory.Cranky,
-        DKBCategory.Seekertone,
-        DKBCategory.Chiptone,
-        DKBCategory.Glamtone
-      ]);
-    });
-
-    test("Divide", () => {
-      expect(getCategoriesForLayer(LayerName.Divide)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Challenge,
-        DKBCategory.Quiztone,
-        DKBCategory.Regular,
-        DKBCategory.Battle,
-        DKBCategory.Cranky,
-        DKBCategory.Glamtone
-      ]);
-    });
-
-    test("Freezer", () => {
-      expect(getCategoriesForLayer(LayerName.Freezer)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Regular,
-        DKBCategory.Battle,
-        DKBCategory.Challenge,
-        DKBCategory.Growtone,
-        DKBCategory.Seekertone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Cranky,
-        DKBCategory.Chiptone,
-        DKBCategory.Smashintone,
-        DKBCategory.Fragmentone,
-        DKBCategory.Rehearsal
-      ]);
-    });
-
-    test("Forest", () => {
-      expect(getCategoriesForLayer(LayerName.Forest)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Battle,
-        DKBCategory.Regular,
-        DKBCategory.Challenge,
-        DKBCategory.Smashintone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Cranky,
-        DKBCategory.Chiptone,
-        DKBCategory.Seekertone,
-        DKBCategory.Fragmentone,
-        DKBCategory.Growtone,
-        DKBCategory.Rehearsal
-      ]);
-    });
-
-    test("Junction", () => {
-      expect(getCategoriesForLayer(LayerName.Junction)).toEqual([
-        DKBCategory.Regular,
-        DKBCategory.Quintuple,
-        DKBCategory.Battle,
-        DKBCategory.Challenge,
-        DKBCategory.Cranky,
-        DKBCategory.ShiftySmash
-      ]);
-    });
-
-    test("Resort", () => {
-      expect(getCategoriesForLayer(LayerName.Resort)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Regular,
-        DKBCategory.Quiztone,
-        DKBCategory.Challenge,
-        DKBCategory.Battle,
-        DKBCategory.Growtone,
-        DKBCategory.Fragmentone,
-        DKBCategory.Smashintone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Cranky,
-        DKBCategory.Bloomintone,
-        DKBCategory.Seekertone,
-        DKBCategory.Glamtone,
-        DKBCategory.Chiptone
-      ]);
-    });
-
-    test("Tempest", () => {
-      expect(getCategoriesForLayer(LayerName.Tempest)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Regular,
-        DKBCategory.Challenge,
-        DKBCategory.Growtone,
-        DKBCategory.Rehearsal,
-        DKBCategory.Battle,
-        DKBCategory.Smashintone,
-        DKBCategory.Seekertone,
-        DKBCategory.Cranky,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Fragmentone
-      ]);
-    });
-
-    test("Landfill", () => {
-      expect(getCategoriesForLayer(LayerName.Landfill)).toEqual([
-        DKBCategory.Regular,
-        DKBCategory.Quintuple,
-        DKBCategory.Bloomintone,
-        DKBCategory.Challenge,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Battle,
-        DKBCategory.Fragmentone,
-        DKBCategory.Cranky,
-        DKBCategory.Smashintone,
-        DKBCategory.Chiptone
-      ]);
-    });
-
-    test("Racing", () => {
-      expect(getCategoriesForLayer(LayerName.Racing)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Battle,
-        DKBCategory.Regular,
-        DKBCategory.Quiztone,
-        DKBCategory.Cranky,
-        DKBCategory.Challenge
-      ]);
-    });
-
-    test("Radiance", () => {
-      expect(getCategoriesForLayer(LayerName.Radiance)).toEqual([
-        DKBCategory.Regular,
-        DKBCategory.Triple,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Challenge,
-        DKBCategory.Battle,
-        DKBCategory.Rehearsal,
-        DKBCategory.Chiptone,
-        DKBCategory.Bloomintone,
-        DKBCategory.Fragmentone,
-        DKBCategory.Smashintone,
-        DKBCategory.Growtone,
-        DKBCategory.Cranky
-      ]);
-    });
-
-    test("Groove", () => {
-      expect(getCategoriesForLayer(LayerName.Groove)).toEqual([
-        DKBCategory.Regular,
-        DKBCategory.Challenge,
-        DKBCategory.Cranky,
-        DKBCategory.Smashintone,
-        DKBCategory.Battle,
-        DKBCategory.Chiptone,
-        DKBCategory.Seekertone
-      ]);
-    });
-
-    test("Feast", () => {
-      expect(getCategoriesForLayer(LayerName.Feast)).toEqual([
-        DKBCategory.Triple,
-        DKBCategory.Regular,
-        DKBCategory.Challenge,
-        DKBCategory.Battle,
-        DKBCategory.Growtone,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Smashintone,
-        DKBCategory.Quiztone,
-        DKBCategory.Fragmentone,
-        DKBCategory.Seekertone,
-        DKBCategory.Chiptone,
-        DKBCategory.Cranky,
-        DKBCategory.Glamtone
-      ]);
-    });
-
-    test("Forbidden", () => {
-      expect(getCategoriesForLayer(LayerName.Forbidden)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Regular,
-        DKBCategory.Challenge,
-        DKBCategory.Battle,
-        DKBCategory.Cranky,
-        DKBCategory.Smashintone,
-        DKBCategory.Chiptone
-      ]);
-    });
-
-    test("Core", () => {
-      expect(getCategoriesForLayer(LayerName.Core)).toEqual([
-        DKBCategory.Quintuple,
-        DKBCategory.Regular,
-        DKBCategory.ShiftySmash,
-        DKBCategory.Smashintone,
-        DKBCategory.Chiptone,
-        DKBCategory.Quiztone,
-        DKBCategory.Cranky,
-        DKBCategory.Triple
-      ]);
+    Object.values(LayerName).forEach((layer: LayerName) => {
+      test(`${layer}`, () => {
+        expect(getCategoriesForLayer(layer)).toHaveLength(expected[layer]);
+      });
     });
   });
 
   describe("getAllBananasForCategories tests", () => {
-    test("No categories, no postgame, no hell mode", () => {
+    test("Categories: 0, Postgame: No, Hell Mode: No", () => {
       expect(
         getBananasForCategories(LayerName.Groove, [], false, false)
       ).toHaveLength(26);
     });
 
-    test("One category, no postgame, no hell mode", () => {
+    test("Categories: 1, Postgame: No, Hell Mode: No", () => {
       expect(
         getBananasForCategories(
           LayerName.Groove,
@@ -360,7 +83,7 @@ describe("layerApi tests", () => {
       ).toHaveLength(18);
     });
 
-    test("One category with postgame, no hell mode", () => {
+    test("Categories: 1, Postgame: Yes, Hell Mode: No", () => {
       expect(
         getBananasForCategories(
           LayerName.Groove,
@@ -371,18 +94,18 @@ describe("layerApi tests", () => {
       ).toHaveLength(20);
     });
 
-    test("One category with postgame and hell mode", () => {
+    test("Categories: 2, Postgame: Yes, Hell Mode: Yes", () => {
       expect(
         getBananasForCategories(
           LayerName.Groove,
-          [DKBCategory.Regular],
+          [DKBCategory.Regular, DKBCategory.Challenge],
           true,
           true
         )
-      ).toHaveLength(21);
+      ).toHaveLength(24);
     });
 
-    test("Multiple categories for layer with at least one category2 banana", () => {
+    test("Check category2", () => {
       expect(
         getBananasForCategories(
           LayerName.Freezer,
--- src/test/utils/levelApi.test.ts
+++ src/test/utils/levelApi.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { DK64CBSanity } from "../../classes";
+import { DK64CBSanity, DK64Item } from "../../classes";
 import { DK64Category, LevelName } from "../../enums";
 import {
   getCategoriesForLevel,
@@ -8,7 +8,7 @@ import {
 } from "../../utils/levelApi";
 
 describe("levelApi tests", () => {
-  const getCBSanity = (
+  const cbSanity = (
     balloons = false,
     bunches = false,
     singles = false
@@ -21,100 +21,66 @@ describe("levelApi tests", () => {
   };
 
   describe("getItemsForLevel tests", () => {
-    test("All", () => {
-      expect(getItemsForLevel(LevelName.All, getCBSanity())).toHaveLength(296);
-    });
+    const expected = {
+      [LevelName.All]: 296,
+      [LevelName.Isles]: 40,
+      [LevelName.Japes]: 35,
+      [LevelName.Aztec]: 36,
+      [LevelName.Factory]: 36,
+      [LevelName.Galleon]: 35,
+      [LevelName.Forest]: 36,
+      [LevelName.Caves]: 35,
+      [LevelName.Castle]: 35,
+      [LevelName.Helm]: 8
+    };
 
-    test("Isles", () => {
-      expect(getItemsForLevel(LevelName.Isles, getCBSanity())).toHaveLength(40);
-    });
-
-    test("Japes", () => {
-      expect(getItemsForLevel(LevelName.Japes, getCBSanity())).toHaveLength(35);
-    });
-
-    test("Aztec", () => {
-      expect(getItemsForLevel(LevelName.Aztec, getCBSanity())).toHaveLength(36);
-    });
-
-    test("Factory", () => {
-      expect(getItemsForLevel(LevelName.Factory, getCBSanity())).toHaveLength(
-        36
-      );
-    });
-
-    test("Galleon", () => {
-      expect(getItemsForLevel(LevelName.Galleon, getCBSanity())).toHaveLength(
-        35
-      );
-    });
-
-    test("Forest", () => {
-      expect(getItemsForLevel(LevelName.Forest, getCBSanity())).toHaveLength(
-        36
-      );
-    });
-
-    test("Caves", () => {
-      expect(getItemsForLevel(LevelName.Caves, getCBSanity())).toHaveLength(35);
-    });
-
-    test("Castle", () => {
-      expect(getItemsForLevel(LevelName.Castle, getCBSanity())).toHaveLength(
-        35
-      );
-    });
-
-    test("Helm", () => {
-      expect(getItemsForLevel(LevelName.Helm, getCBSanity())).toHaveLength(8);
+    Object.values(LevelName).forEach((level: LevelName) => {
+      test(`${level}`, () => {
+        expect(getItemsForLevel(level, cbSanity())).toHaveLength(
+          expected[level]
+        );
+      });
     });
 
     describe("CBSanity checks", () => {
+      const noMedals = (list: DK64Item[]) => {
+        expect(
+          list.filter((item) => item.category === DK64Category.Medal)
+        ).toHaveLength(0);
+      };
+
       test("Balloons", () => {
-        const japes = getItemsForLevel(LevelName.Japes, getCBSanity(true));
-        expect(japes).toHaveLength(43);
+        const japes = getItemsForLevel(LevelName.Japes, cbSanity(true));
         expect(
           japes.filter((item) => item.category === DK64Category.Balloon)
         ).toHaveLength(13);
-        expect(
-          japes.filter((item) => item.category === DK64Category.Medal)
-        ).toHaveLength(0);
+        noMedals(japes);
       });
 
       test("Bunches", () => {
-        const japes = getItemsForLevel(
-          LevelName.Japes,
-          getCBSanity(false, true)
-        );
-        expect(japes).toHaveLength(63);
+        const japes = getItemsForLevel(LevelName.Japes, cbSanity(false, true));
         expect(
           japes.filter((item) => item.category === DK64Category.Bunch)
         ).toHaveLength(33);
-        expect(
-          japes.filter((item) => item.category === DK64Category.Medal)
-        ).toHaveLength(0);
+        noMedals(japes);
       });
 
       test("Singles", () => {
         const japes = getItemsForLevel(
           LevelName.Japes,
-          getCBSanity(false, false, true)
+          cbSanity(false, false, true)
         );
-        expect(japes).toHaveLength(47);
         expect(
           japes.filter((item) => item.category === DK64Category.Single)
         ).toHaveLength(17);
-        expect(
-          japes.filter((item) => item.category === DK64Category.Medal)
-        ).toHaveLength(0);
+        noMedals(japes);
       });
 
       test("Medals not removed when CBSanity does not apply", () => {
         const helm = getItemsForLevel(
           LevelName.Helm,
-          getCBSanity(true, true, true)
+          cbSanity(true, true, true)
         );
-        expect(helm).toHaveLength(8);
         expect(
           helm.filter((item) => item.category === DK64Category.HelmMedal)
         ).toHaveLength(5);
@@ -123,162 +89,105 @@ describe("levelApi tests", () => {
   });
 
   describe("getCategoriesForLevel tests", () => {
-    test("All", () => {
-      expect(getCategoriesForLevel(LevelName.All, getCBSanity())).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.CompanyCoin,
-        DK64Category.RainbowCoin,
-        DK64Category.Medal,
-        DK64Category.Key,
-        DK64Category.HelmMedal
-      ]);
+    const expected = {
+      [LevelName.All]: 8,
+      [LevelName.Isles]: 5,
+      [LevelName.Japes]: 6,
+      [LevelName.Aztec]: 6,
+      [LevelName.Factory]: 7,
+      [LevelName.Galleon]: 6,
+      [LevelName.Forest]: 6,
+      [LevelName.Caves]: 6,
+      [LevelName.Castle]: 6,
+      [LevelName.Helm]: 4
+    };
+
+    Object.values(LevelName).forEach((level: LevelName) => {
+      test(`${level}`, () => {
+        expect(getCategoriesForLevel(level, cbSanity())).toHaveLength(
+          expected[level]
+        );
+      });
     });
 
-    test("Isles", () => {
-      expect(getCategoriesForLevel(LevelName.Isles, getCBSanity())).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.CompanyCoin,
-        DK64Category.RainbowCoin
-      ]);
-    });
+    describe("CBSanity checks", () => {
+      const noMedals = (list: DK64Category[]) => {
+        expect(list.filter((item) => item === DK64Category.Medal)).toHaveLength(
+          0
+        );
+      };
 
-    test("Japes", () => {
-      expect(getCategoriesForLevel(LevelName.Japes, getCBSanity())).toEqual([
-        DK64Category.GB,
-        DK64Category.Medal,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin
-      ]);
-    });
+      test("Balloons", () => {
+        const galleon = getCategoriesForLevel(
+          LevelName.Galleon,
+          cbSanity(true)
+        );
+        expect(galleon).toHaveLength(6);
+        noMedals(galleon);
+      });
 
-    test("Aztec", () => {
-      expect(getCategoriesForLevel(LevelName.Aztec, getCBSanity())).toEqual([
-        DK64Category.GB,
-        DK64Category.Medal,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin
-      ]);
-    });
+      test("Bunches", () => {
+        const galleon = getCategoriesForLevel(
+          LevelName.Galleon,
+          cbSanity(false, true)
+        );
+        expect(galleon).toHaveLength(6);
+        noMedals(galleon);
+      });
 
-    test("Factory", () => {
-      expect(getCategoriesForLevel(LevelName.Factory, getCBSanity())).toEqual([
-        DK64Category.GB,
-        DK64Category.Medal,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.CompanyCoin,
-        DK64Category.RainbowCoin
-      ]);
-    });
+      test("Singles", () => {
+        const galleon = getCategoriesForLevel(
+          LevelName.Galleon,
+          cbSanity(false, false, true)
+        );
+        expect(galleon).toHaveLength(6);
+        noMedals(galleon);
+      });
 
-    test("Galleon (with balloons)", () => {
-      expect(
-        getCategoriesForLevel(LevelName.Galleon, getCBSanity(true))
-      ).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin,
-        DK64Category.Balloon
-      ]);
-    });
-
-    test("Forest (with bunches)", () => {
-      expect(
-        getCategoriesForLevel(LevelName.Forest, getCBSanity(false, true))
-      ).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin,
-        DK64Category.Bunch
-      ]);
-    });
-
-    test("Caves (with singles)", () => {
-      expect(
-        getCategoriesForLevel(LevelName.Caves, getCBSanity(false, false, true))
-      ).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin,
-        DK64Category.Single
-      ]);
-    });
-
-    test("Castle (with all CBSanity)", () => {
-      expect(
-        getCategoriesForLevel(LevelName.Castle, getCBSanity(true, true, true))
-      ).toEqual([
-        DK64Category.GB,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key,
-        DK64Category.RainbowCoin,
-        DK64Category.Balloon,
-        DK64Category.Bunch,
-        DK64Category.Single
-      ]);
-    });
-
-    test("Helm", () => {
-      expect(
-        getCategoriesForLevel(LevelName.Helm, getCBSanity(true, true, true))
-      ).toEqual([
-        DK64Category.HelmMedal,
-        DK64Category.Crown,
-        DK64Category.Fairy,
-        DK64Category.Key
-      ]);
+      test("All", () => {
+        const galleon = getCategoriesForLevel(
+          LevelName.Galleon,
+          cbSanity(true, true, true)
+        );
+        expect(galleon).toHaveLength(8);
+        noMedals(galleon);
+      });
     });
   });
 
   describe("getItemsForCategories tests", () => {
-    test("No categories, no hell mode", () => {
+    test("Categories: 0, Hell Mode: No, CBSanity: None", () => {
       expect(
-        getItemsForCategories(LevelName.Factory, [], false, getCBSanity())
+        getItemsForCategories(LevelName.Factory, [], false, cbSanity())
       ).toHaveLength(34);
     });
 
-    test("No categories, hell mode", () => {
+    test("Categories: 0, Hell Mode: Yes, CBSanity: Balloons", () => {
       expect(
-        getItemsForCategories(LevelName.Factory, [], true, getCBSanity())
-      ).toHaveLength(36);
+        getItemsForCategories(LevelName.Factory, [], true, cbSanity(true))
+      ).toHaveLength(44);
     });
 
-    test("One category, no hell mode", () => {
+    test("Categories: 1, Hell Mode: No, CBSanity: Bunches", () => {
       expect(
         getItemsForCategories(
           LevelName.Factory,
           [DK64Category.GB],
           false,
-          getCBSanity()
+          cbSanity(false, true)
         )
       ).toHaveLength(24);
     });
 
-    test("Multiple categories, no hell mode", () => {
+    test("Categories: 2, Hell Mode: No, CBSanity: All", () => {
       expect(
         getItemsForCategories(
           LevelName.Factory,
-          [DK64Category.Fairy, DK64Category.Crown],
+          [DK64Category.Fairy, DK64Category.Crown, DK64Category.Single],
           false,
-          getCBSanity()
+          cbSanity(true, true, true)
         )
-      ).toHaveLength(3);
+      ).toHaveLength(18);
     });
   });
 });
